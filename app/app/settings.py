87c48dda336b0ea8ea2b3c6feac41ca4ad07e7cc
Resad <resad.hajdarpasic@gmail.com>
2020-01-12 18:07:21 +0100

    feat(user): implement user create endpoint

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6sufgga2a*9i$l30g84bn36n*^!bm0tz&t-*%%c1vurj(&ck8%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
