6ad64f6073ee8e674e65e3b78ab0c2d7569fe466
Resad <resad.hajdarpasic@gmail.com>
2020-02-02 21:01:17 +0100

    feat: add create ingredient feature

--- recipe/tests/test_ingredients_api.py
+++ recipe/tests/test_ingredients_api.py
@@ -20,6 +20,7 @@ class PublicIngredientsApiTests(TestCase):
 
     def test_login_required(self):
         """Test that login is required to access the endoint"""
+
         res = self.client.get(INGREDIENTS_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
@@ -58,3 +59,19 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    def test_create_ingredient_successful(self):
+        """Test create a new ingredient"""
+        payload = {'name': 'Cabbage'}
+        self.client.post(INGREDIENTS_URL, payload)
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_ingredients_invalid(self):
+        """Test creating invalid ingredient fails"""
+        payload = {'name': ''}
+        res = self.client.post(INGREDIENTS_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
